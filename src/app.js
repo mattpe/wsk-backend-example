9926748589882e1daa6aa658826122b1c56088f0
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2025-11-06 14:32:01 +0200

    clean mockup data

--- src/app.js
+++ src/app.js
@@ -2,27 +2,6 @@ import express from 'express';
 import api from './api/index.js';
 const app = express();
 
-// mock-data
-
-const cats = [
-  {
-    cat_id: 2,
-    name: 'Kisu',
-    birthdate: '2023-10-08',
-    weight: 6,
-    owner: 'Hessu',
-    image: 'https://loremflickr.com/320/240/cat',
-  },
-  {
-    cat_id: 3,
-    name: 'Misu',
-    birthdate: '2021-11-18',
-    weight: 7,
-    owner: 'Hessu',
-    image: 'https://loremflickr.com/320/240/cat3',
-  },
-];
-
 // Web sivusto tarjoillaan public-kansiosta
 //app.use('/sivusto', express.static('public')); // aliosoite /sivusto
 // tai palvelimen juuri /
@@ -35,27 +14,5 @@ app.use(express.urlencoded({extended: true}));
 
 // lisää prefixin ja ohjaa siten kaikkki api-routerin sisällä oleville reiteille
 app.use('/api/v1', api);
-
-// '/api'-polun juuri
-app.get('/api/v1', (req, res) => {
-  res.send('Welcome to my REST API!');
-});
-
-app.get('/api/v1/cats/:id', (req, res) => {
-  //console.log('cat id', req.params.id);
-  const cat = cats.find((cat) => cat.cat_id === parseInt(req.params.id));
-  if (cat) {
-    res.json(cat);
-  } else {
-    //res.sendStatus(404);
-    res.status(404).json({message: 'cat not found'});
-  }
-});
-
-app.post('/api/v1/cats', (req, res) => {
-  console.log(req.body);
-  // TODO: add posted cat to data
-  res.sendStatus(201);
-});
 
 export default app;
