9a323438114a431b8e04b1e3a57772a0db7c7166
mattpe <mattpe@metropolia.fi>
2025-11-07 10:22:20 +0200

    cat db model added

--- src/api/controllers/cat-controller.js
+++ src/api/controllers/cat-controller.js
@@ -1,11 +1,11 @@
 import {addCat, findCatById, listAllCats} from '../models/cat-model.js';
 
-const getCat = (req, res) => {
-  res.json(listAllCats());
+const getCat = async (req, res) => {
+  res.json(await listAllCats());
 };
 
-const getCatById = (req, res) => {
-  const cat = findCatById(req.params.id);
+const getCatById = async (req, res) => {
+  const cat = await findCatById(req.params.id);
   if (cat) {
     res.json(cat);
   } else {
@@ -13,14 +13,14 @@ const getCatById = (req, res) => {
   }
 };
 
-const postCat = (req, res) => {
+const postCat = async (req, res) => {
   console.log(req.body);
   console.log(req.file);
   console.log(req.file.filename);
   // lisätään tiedostonimi req.bodyyn, jotta addCat saa kaiken
   req.body.filename = req.file.filename;
 
-  const result = addCat(req.body);
+  const result = await addCat(req.body);
   if (result.cat_id) {
     res.status(201);
     res.json({message: 'New cat added.', result});
@@ -30,13 +30,13 @@ const postCat = (req, res) => {
 };
 
 const putCat = (req, res) => {
-  // not implemented in this example, this is future homework
+  // not implemented in this example, this is homework
 
   res.status(200).json({message: 'Cat item updated.'});
 };
 
 const deleteCat = (req, res) => {
-  // not implemented in this example, this is future homework
+  // not implemented in this example, this is homework
   res.status(200);
   res.json({message: 'Cat item deleted.'});
 };
--- src/api/models/cat-model.js
+++ src/api/models/cat-model.js
@@ -1,43 +1,51 @@
-// mock data
-const catItems = [
-  {
-    cat_id: 9592,
-    cat_name: 'Frank',
-    weight: 11,
-    owner: 3609,
-    filename: 'f3dbafakjsdfhg4',
-    birthdate: '2021-10-12',
-  },
-  {
-    cat_id: 9590,
-    cat_name: 'Mittens',
-    weight: 8,
-    owner: 3602,
-    filename: 'f3dasdfkjsdfhgasdf',
-    birthdate: '2021-10-12',
-  },
-];
+import promisePool from '../../utils/database.js';
 
-const listAllCats = () => {
-  return catItems;
+const listAllCats = async () => {
+    const result = await promisePool.query('SELECT * FROM wsk_cats');
+    //console.log('result', result);
+    const rows = result[0];
+    return rows;
 };
 
-const findCatById = (id) => {
-  return catItems.find((item) => item.cat_id == id);
+const findCatById = async (id) => {
+    const [rows] = await promisePool.execute('SELECT * FROM wsk_cats WHERE cat_id = ?', [id]);
+    console.log('rows', rows);
+     if (rows.length === 0) {
+        return false;
+     }
+     return rows[0];
 };
 
-const addCat = (cat) => {
+const addCat = async (cat) => {
   const {cat_name, weight, owner, filename, birthdate} = cat;
-  const newId = catItems[0].cat_id + 1;
-  catItems.unshift({
-    cat_id: newId,
-    cat_name,
-    weight,
-    owner,
-    filename,
-    birthdate,
-  });
-  return {cat_id: newId};
+  const sql = `INSERT INTO wsk_cats (cat_name, weight, owner, filename, birthdate)
+               VALUES (?, ?, ?, ?, ?)`;
+  const params = [cat_name, weight, owner, filename, birthdate];
+    const result = await promisePool.execute(sql, params);
+    console.log('result', result);
+     if (result[0].affectedRows === 0) {
+        return false;
+     }
+    return {cat_id: result[0].insertId};
 };
 
-export {listAllCats, findCatById, addCat};
+const modifyCat = async (cat, id) => {
+  const sql = promisePool.format(`UPDATE wsk_cats SET ? WHERE cat_id = ?`, [cat, id]);
+    const rows = await promisePool.execute(sql);
+    console.log('rows', rows);
+     if (rows[0].affectedRows === 0) {
+        return false;
+     }
+     return {message: 'success'};
+};
+
+const removeCat = async (id) => {
+    const [rows] = await promisePool.execute('DELETE FROM wsk_cats WHERE cat_id = ?', [id]);
+    console.log('rows', rows);
+     if (rows.affectedRows === 0) {
+        return false;
+     }
+     return {message: 'success'};
+};
+
+export {listAllCats, findCatById, addCat, modifyCat, removeCat};
