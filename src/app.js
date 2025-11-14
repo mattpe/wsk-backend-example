9e810e5b04c6e4a7f0ed1ab56c78c8523e846293
mattpe <mattpe@users.noreply.github.com>
2025-11-14 09:21:51 +0200

    Update API version prefix from v2 to v3 conflict test

--- src/app.js
+++ src/app.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(express.static('public'));
 
 // parsii json-datan http-pyynnöstä
-console.log('Moi');
+console.log('Moi taas');
 app.use(express.json());
 // formdataa varten
 app.use(express.urlencoded({extended: true}));
 
 // lisää prefixin ja ohjaa siten kaikkki api-routerin sisällä oleville reiteille
-app.use('/api/v2', api);
+app.use('/api/v3', api);
 
 
 export default app;
